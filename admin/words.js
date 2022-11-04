89e566782785c257f418e1cab773f0eb8ad75b2d
ioBroker Adapter Creator <noreply@iobroker.net>
2022-11-04 09:56:55 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,67 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'combustion-control adapter settings': {
+		'en': 'Adapter settings for combustion-control',
+		'de': 'Adaptereinstellungen für combustion-control',
+		'ru': 'Настройки адаптера для combustion-control',
+		'pt': 'Configurações do adaptador para combustion-control',
+		'nl': 'Adapterinstellingen voor combustion-control',
+		'fr': "Paramètres d'adaptateur pour combustion-control",
+		'it': "Impostazioni dell'adattatore per combustion-control",
+		'es': 'Ajustes del adaptador para combustion-control',
+		'pl': 'Ustawienia adaptera dla combustion-control',
+		'zh-cn': 'combustion-control的适配器设置'
+	},
+	'macAddress': {
+		'en': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'de': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'ru': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'pt': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'nl': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'fr': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'it': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'es': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'pl': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)",
+		'zh-cn': "Bluetooth device MAC address (if empty, first found 'oControl' device will be used.)"
+	},
+	'deviceName': {
+		'en': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'de': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'ru': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'pt': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'nl': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'fr': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'it': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'es': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'pl': "Bluetooth device name (if empty, default name 'oControl' will be used.))",
+		'zh-cn': "Bluetooth device name (if empty, default name 'oControl' will be used.))"
+	},
+	'updateIntervall': {
+		'en': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'de': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'ru': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'pt': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'nl': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'fr': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'it': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'es': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'pl': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)',
+		'zh-cn': 'Update interval in seconds (minimu is 5 seconds. Default is 60 seconds.)'
+	},
+	'additionalMessages': {
+		'en': 'More logging messages',
+		'de': 'More logging messages',
+		'ru': 'More logging messages',
+		'pt': 'More logging messages',
+		'nl': 'More logging messages',
+		'fr': 'More logging messages',
+		'it': 'More logging messages',
+		'es': 'More logging messages',
+		'pl': 'More logging messages',
+		'zh-cn': 'More logging messages'
+	}
+};
